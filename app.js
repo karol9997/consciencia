bf96467bb0ee3b3b8fd91a5f4abeac560db893f9
karol9997 <ana.morais.brasil@escola.pr.gov.br>
2025-11-11 18:39:19 -0300

    a

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById("musicasBtn").addEventListener("click", function() {
+    alert(`🎧 Músicas famosas de Frank Ocean:
+1. Nikes
+2. Thinkin Bout You
+3. Pink + White
+4. Ivy
+5. Nights`);
+});
+
+document.getElementById("imagensBtn").addEventListener("click", function() {
+    alert("🖼️ As imagens mostram diferentes fases da carreira de Frank Ocean, incluindo shows e sessões de estúdio.");
+});
